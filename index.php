65aff6db42132abe12f22f96236ec87de4878d55
Rosa <rosa.morales.rojas@gmail.com>
2018-07-21 19:49:19 -0400

    se realiza modifica la estructura del sitio web

--- index.php
+++ index.php
@@ -4,15 +4,7 @@
 
 <!-- The loop -->
 
-<?php if(have_posts()): ?>
-	<?php while(have_posts()): ?>
-		<?php the_post(); ?>
-		<div class="post">
-			<h1 class="post__title"><?php the_title(); ?></h1>
-			<div class="post__content"><?php the_content(); ?></div>
-		</div>
-	<?php endwhile; ?>
-<?php endif; ?>
+
 
 <!-- /The loop -->
 
